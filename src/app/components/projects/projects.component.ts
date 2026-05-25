f746b34cdbf2c71e3b3a922238d47a85ba57ade5
angelmg777 <agmg.angel@gmail.com>
2026-05-25 14:40:30 -0600

    New projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -21,9 +21,29 @@ export class ProjectsComponent {
       image: 'assets/Images/Fox-shop.png'
     },
     {
+      title: 'Menu Digital - Ruso Bar',
+      description: 'Aplicación web fullstack para la gestión y visualización del menú de un bar. Permite a los clientes explorar bebidas con filtros dinámicos, y al administrador gestionar el catálogo completo desde un panel protegido',
+      tags: ['React', 'Tailwind CSS', 'JavaScript', 'Node.js', 'MongoDB', 'Vercel', 'Railway', 'Express', 'JWT-Bcryptjs', 'Vite'],
+      github: 'https://github.com/angelmg777/menu-bar',
+      live: 'https://menu-bar-pied.vercel.app/',
+      featured: false,
+      color: 'linear-gradient(135deg, #0a0e1a, #1a0a2e)',
+      image: 'assets/Images/WebMenuBar.png'
+    },
+    {
+      title: 'Finanzas Web',
+      description: 'Aplicación web fullstack de gestión financiera personal con autenticación completa, dashboard con gráficas interactivas y diseño responsive',
+      tags: ['React', 'TypeScript', 'Vite', 'TailwindCSS', 'Node.js', 'Express', 'Prisma ORM', 'PostgreSQL', 'JWT-Bcryptjs', 'Brevo','Vercel', 'Railway' ],
+      github: 'https://github.com/angelmg777/Finanzas-App',
+      live: 'https://finanzas-app-eight-jade.vercel.app/',
+      featured: true,
+      color: 'linear-gradient(135deg, #0a0e1a, #1a0a2e)',
+      image: 'assets/Images/WebFinanzas.png'
+    },
+    {
       title: 'StudentRent  Aplicación móvil',
       description: 'Desarrollo de app multiplataforma para renta de alojamientos para estudiantes. Implementación de registro de usuarios, navegación, gestión de datos y filtros avanzados ',
-      tags: ['React Nativve', 'Firebase', 'Git', 'JavaScript','Maps SDK'],
+      tags: ['React Native', 'Firebase', 'Git', 'JavaScript','Maps SDK', 'Expo'],
       github: 'https://github.com/angelmg777',
       featured: false,
       color: 'linear-gradient(135deg, #0a1a1a, #001a2e)',
@@ -33,11 +53,13 @@ export class ProjectsComponent {
     {
       title: 'Portafolio Web',
       description: 'Portafolio web personal con informacion sobre experiencia, habilidades y datos de contacto. Implementa animaciones con Canvas API, IntersectionObserver y efectos CSS avanzados.',
-      tags: ['Angular', 'Node.js', 'Typescript', 'Vercel' ],
+      tags: ['Angular', 'Typescript', 'Vercel' ],
       github: 'https://github.com/angelmg777/Portafolio-Personal',
+      live : 'https://portafolio-personal-mu-hazel.vercel.app/',
       featured: false,
       color: 'linear-gradient(135deg, #0a1a0a, #001a10)',
       image: 'assets/Images/portafolio.png'
     }
+
   ];
 }
